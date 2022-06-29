49aabfce9affe8de9bbd8616462e4f539c196b3c
ksenijacer <ksenijacerovic7@gmail.com>
2022-06-29 19:41:41 +0200

    Zadatak 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
+import AppPosts from './pages/AppPosts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Router>
+      <Link to='/posts'>Posts</Link>
+      <Route path='/posts'>
+       <AppPosts />
+      </Route>
+    </Router>
+  </div>
   );
 }
 
---
+++ src/pages/AppPosts.js
@@ -0,0 +1,10 @@
+import App from "../App";
+function AppPosts() {
+    return(
+        <div>
+            App Posts
+        </div>
+    )
+}
+
+export default AppPosts;
